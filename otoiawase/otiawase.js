5bb3edc6da8eebd95999bb2652e3e5292c0b70df
0uga <KTC24A31C0002@edu.kyoto-tech.ac.jp>
2024-08-31 00:54:48 +0900

    編集

--- otoiawase/otiawase.js
+++ otoiawase/otiawase.js
@@ -1,13 +1,3 @@
-script.js
-document.addEventListener("DOMContentLoaded", function () {
-  const contactForm = document.getElementById("contact-form");
-  contactForm.addEventListener("submit", function (e) {
-    e.preventDefault();
-    const formData = new FormData(contactForm);
-    alert("お問い合わせが送信されました！");
-  });
-});
-
 bubbly({
   bubbles: {},
 });
